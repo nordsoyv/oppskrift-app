442fbfcda6010d19ffc854e0afd55bd3e9e81325
Oyvind Nordsveen <oyvind.nordsveen@gmail.com>
2013-03-04 09:54:09 +0100

    Followed tutorial from http://briantford.com/blog/angular-express.html

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = module.exports = express();
 
 // Configuration
 
-app.configure(function(){
+app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -20,11 +20,11 @@ app.configure(function(){
   app.use(app.router);
 });
 
-app.configure('development', function(){
+app.configure('development', function (){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function (){
   app.use(express.errorHandler());
 });
 
@@ -35,7 +35,12 @@ app.get('/partials/:name', routes.partials);
 
 // JSON API
 
-app.get('/api/name', api.name);
+app.get('/api/posts', api.posts);
+
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
