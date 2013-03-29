802cd988a92159f1150eeeda8974dd8d88ba5923
Oyvind Nordsveen <oyvind.nordsveen@gmail.com>
2013-03-29 22:14:27 +0100

    Fixed editing of steps

--- db/index.js
+++ db/index.js
@@ -92,13 +92,13 @@ exports.getAllPosts = function(){
 var post1 = {
   title : "Lorem ipsum",
   text : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  steps : ["nr1","nr2"]
+  steps : [{text:"nr1"},{text:"nr2"}]
 };
 
 var post2 = {
   title : "Sed egestas",
   text : "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus.",
-  steps:  ["nr3","nr4"]
+  steps:  [{text:"nr3"},{text:"nr4"}]
 };
 
 exports.addPost(post1);
