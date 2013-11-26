ff814e8101d0c4efbf3fbd395f573901588e395a
Oyvind Nordsveen <oyvind.nordsveen@visma.com>
2013-11-26 15:38:42 +0100

    Revert "Added ejs dependency"
    This reverts commit 34453c6110d7e588bc105931a151ca5b3131532c.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+
 /**
  * Module dependencies.
  */
 
 var express = require('express'),
-    routes = require('./routes'),
-    api = require('./routes/api'),
-    winston = require('winston');
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  winston = require('winston');
 
 var app = module.exports = express();
 
@@ -13,30 +14,30 @@ var app = module.exports = express();
 
 winston.remove(winston.transports.Console);
 //winston.add(winston.transports.Console, { level : "error"  });
-winston.add(winston.transports.Console, { level: "info"  });
+winston.add(winston.transports.Console, { level : "info"  });
 
 app.configure(function () {
-    app.set('views', __dirname + '/views');
-//  app.set('view engine', 'jade');
-    app.engine('html', require('ejs').renderFile);
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use(app.router);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
-app.configure('development', function () {
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function (){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function () {
-    app.use(express.errorHandler());
+app.configure('production', function (){
+  app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
+
 
 
 // JSON API
@@ -52,6 +53,6 @@ app.get('*', routes.index);
 
 // Start server
 
-app.listen(3000, function (err) {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(3000, function(err){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
